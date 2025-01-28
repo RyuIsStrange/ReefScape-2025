fe8c173d35b76f434a635eaa243b0d9f86ededde
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2025-01-28 13:07:04 -0600

    fix: field no like update

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -50,7 +50,7 @@ public class Vision {
   /**
    * April Tag Field Layout of the year.
    */
-  public static final AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+  public static final AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
 
   /**
    * Ambiguity defined as a value between (0,1). Used in {@link Vision#filterPose}.
