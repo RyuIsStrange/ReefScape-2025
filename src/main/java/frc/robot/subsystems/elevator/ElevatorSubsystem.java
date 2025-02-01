144bba00b36a519e9348e8b938e44610f4c7c005
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2025-01-31 22:08:32 -0600

    worked on elevator subsystems

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -7,7 +7,10 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ElevatorSubsystem extends SubsystemBase {
     private final SparkMax m_Elevator;
@@ -15,7 +18,7 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     public ElevatorSubsystem() {
         // Define the motor
-        m_Elevator = new SparkMax(13, MotorType.kBrushless);
+        m_Elevator = new SparkMax(Constants.SubMotorIDs.kElevatorID, MotorType.kBrushless);
 
         // Define the Encoder
 
@@ -23,17 +26,39 @@ public class ElevatorSubsystem extends SubsystemBase {
         config
             // Deal with things with idle mode/follower first
             .idleMode(IdleMode.kBrake)
-
+        
             /* 
              * Move onto the encoder
              * Leave commented out till we decide if we should be using IPS or FPS
             */
             // .absoluteEncoder
-                // .positionConversionFactor(0) // need math for this. Rotations to IN/FT? (SpoolRadius * 2 * Math.PI)?
-                // .velocityConversionFactor(0) // change RPM to Inch Per Sec / Feet Per Sec? (SpoolRadius * 2 * Math.PI) / 60?
+                // .positionConversionFactor(0) // need math for this. Rotations to IN/FT? RotationToMeters? -> (SpoolRadius * 2 * Math.PI)?
+                // .velocityConversionFactor(0) // change RPM to Inch Per Sec / Feet Per Sec? RPM-MPS? -> (SpoolRadius * (2 * Math.PI)) / 60?
             ; // Leave this here to make config happy
 
         // Set the motor configuration
         m_Elevator.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    public static class E_PIDF {
+        public static final Constraints Constraints = new Constraints(0, 0);
+
+        public static final double FeedForward = 0;
+
+        public static final double Proportion = 0;
+
+        public static final double Integral = 0;
+
+        public static final double Derivative = 0;
+    }
+
+
+
+
+
+    public Command ManualStop() {
+        return run(() -> 
+            m_Elevator.set(0)
+        );
     }
 }
