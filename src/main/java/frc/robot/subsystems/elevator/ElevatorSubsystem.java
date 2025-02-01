9c3e0d76f3aa73a3ca63675c67622fe0d79848ea
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2025-02-01 00:10:36 -0600

    work?

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.elevator;
 
+import java.util.function.DoubleSupplier;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -7,6 +8,8 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -16,17 +19,18 @@ public class ElevatorSubsystem extends SubsystemBase {
     private final SparkMax m_Elevator;
     private final SparkMaxConfig config = new SparkMaxConfig();
 
+    private final ProfiledPIDController m_elePIDController = new ProfiledPIDController(E_PIDF.Proportion, E_PIDF.Integral, E_PIDF.Derivative, E_PIDF.Constraints);
+
     public ElevatorSubsystem() {
         // Define the motor
         m_Elevator = new SparkMax(Constants.SubMotorIDs.kElevatorID, MotorType.kBrushless);
-
-        // Define the Encoder
 
         // Set the motor configs
         config
             // Deal with things with idle mode/follower first
             .idleMode(IdleMode.kBrake)
         
+            .smartCurrentLimit(20)
             /* 
              * Move onto the encoder
              * Leave commented out till we decide if we should be using IPS or FPS
@@ -38,10 +42,13 @@ public class ElevatorSubsystem extends SubsystemBase {
 
         // Set the motor configuration
         m_Elevator.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        m_elePIDController.setTolerance(0.003);
+        m_elePIDController.calculate(0);
     }
 
     public static class E_PIDF {
-        public static final Constraints Constraints = new Constraints(0, 0);
+        public static final Constraints Constraints = new Constraints(0.3, 0.3);
 
         public static final double FeedForward = 0;
 
@@ -52,9 +59,26 @@ public class ElevatorSubsystem extends SubsystemBase {
         public static final double Derivative = 0;
     }
 
+    public double getEncoderPos() {
+        return m_Elevator.getAbsoluteEncoder().getPosition();
+    }
 
+    public Command runElevator(double setpoint) {
+        m_elePIDController.setGoal(setpoint);
 
+        return run(() -> {
+            m_Elevator.set(m_elePIDController.calculate(getEncoderPos(), setpoint));
+        })
+        .until(() -> m_elePIDController.atSetpoint() && MathUtil.isNear(setpoint, getEncoderPos(), 0.003))
+        .andThen(runOnce(() -> m_Elevator.set(0)));
+    }
 
+    public Command ManualRun(DoubleSupplier supplier) {
+        double power = supplier.getAsDouble();
+        return run(() ->
+            m_Elevator.set(power)
+        );
+    }
 
     public Command ManualStop() {
         return run(() -> 
@@ -62,3 +86,4 @@ public class ElevatorSubsystem extends SubsystemBase {
         );
     }
 }
+
