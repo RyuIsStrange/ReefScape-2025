7672900a546ca610cec566baf3eb15a67bb34402
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2025-02-06 13:31:39 -0600

    feat: limit switch max

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -18,7 +19,12 @@ import frc.robot.Constants;
 public class ElevatorSubsystem extends SubsystemBase {
     private final SparkMax m_Elevator;
     private final SparkMaxConfig config = new SparkMaxConfig();
-    private final DigitalInput m_bottomLimit = new DigitalInput(0); 
+    private final DigitalInput m_bottomLimit = new DigitalInput(0);
+    private final DigitalInput m_L1 = new DigitalInput(1);
+    private final DigitalInput m_L2 = new DigitalInput(2);
+    private final DigitalInput m_L3 = new DigitalInput(3);
+    private final DigitalInput m_L4 = new DigitalInput(4);
+    private final DigitalInput m_upperLimit = new DigitalInput(5);
 
     public ElevatorSubsystem() {
         m_Elevator = new SparkMax(Constants.SubMotorIDs.kElevatorID, MotorType.kBrushless);
@@ -26,17 +32,44 @@ public class ElevatorSubsystem extends SubsystemBase {
         m_Elevator.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
-    public Command runElev(double setpoint) {
-        return run(() -> {
-            //See how far off you are
-            var error = m_Elevator.getEncoder().getPosition() - setpoint;
-            //calculate what motor power you need
-            var kp_gain = 0.01; 
-            var output = error*kp_gain;
-            m_Elevator.set(output);
-        });
+    public BooleanSupplier switchStates(DigitalInput limitSwitch) {
+        return () -> limitSwitch.get() == false;
     }
-    
+
+    public Command runElevBtm() {
+        return runOnce(() -> {
+            m_Elevator.set(-0.2);
+        }).until(switchStates(m_bottomLimit))
+        .andThen(ManualStop());
+    }
+
+    public Command runElevL1() {
+        return runOnce(() -> 
+            m_Elevator.set(.2)
+        ).until(switchStates(m_L1))
+        .andThen(ManualStop());
+    }
+    public Command runElevL2() {
+        return run(() -> {
+            m_Elevator.set(.2);
+        }).until(switchStates(m_L2))
+        .andThen(ManualStop());
+    }
+    public Command runElevL3() {
+        return run(() -> {
+            m_Elevator.set(.2);
+        }).until(switchStates(m_L3))
+        .andThen(ManualStop());
+    }
+    public Command runElevL4() {
+        return run(() -> {
+            m_Elevator.set(.2);
+        }).until(switchStates(m_L4))
+        .andThen(ManualStop());
+    }
+
+    // Stop
+
     public Command ManualStop() {
         return run(() -> 
             m_Elevator.set(0)
@@ -47,7 +80,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     public void periodic() {
         SmartDashboard.putNumber("Elevator Encoder", m_Elevator.getEncoder().getPosition());
         
-        if (m_bottomLimit.get()) {
+        if (m_bottomLimit.get() || m_upperLimit.get()) {
             m_Elevator.getEncoder().setPosition(0);
         }
     }
