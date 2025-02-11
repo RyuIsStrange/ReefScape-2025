67cb0cb6f7fccfccaa73f4213034ab7f4514c5cb
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2025-02-11 13:05:59 -0600

    chore: cleaned up robo container

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,23 +4,22 @@
 
 package frc.robot;
 
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import swervelib.math.Matter;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
- * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
- * not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants
 {
   public static final CommandXboxController driverController = new CommandXboxController(DriveteamConstants.kDriverControllerPort);
   public static final CommandXboxController operatorController = new CommandXboxController(DriveteamConstants.kOperatorControllerPort);
+
+  public static final Optional<Alliance> alliance = DriverStation.getAlliance();
 
   public static class DriveteamConstants {
     public static final int kDriverControllerPort = 0;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,21 +27,91 @@ import frc.robot.subsystems.ElevatorSubsystem;
 // import frc.robot.subsystems.ElevatorSubsystemEnc; req subsystem code isn't in github
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+
 import java.io.File;
+import java.util.function.Supplier;
 import swervelib.SwerveInputStream;
 
 
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/neo"));
-
   final CommandXboxController driverXbox = new CommandXboxController(0);
-
+  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/neo"));
   final ElevatorSubsystem m_elevator = new ElevatorSubsystem();
   final ShooterSubsystem m_shooter = new ShooterSubsystem();
   // final ElevatorSubsystemEnc m_elevatorEnc = new ElevatorSubsystemEnc(); req subsystem code isn't in github
-
   private final SendableChooser<Command> autoChooser;
+
+  public RobotContainer()
+  {
+    // Get our auto commands.
+    pathplannerCommands();
+
+    DriverStation.silenceJoystickConnectionWarning(true);
+
+    // Create the autochooser for the driver station dashboard
+    autoChooser = AutoBuilder.buildAutoChooser("New Path");
+    SmartDashboard.putData("Auto Mode", autoChooser);
+
+    // Forward port 5800 for photonvision
+    PortForwarder.add(5800, "photonvision.local", 5800);
+
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+  private void configureBindings() {
+    setDriveMode();
+
+    Constants.driverController.a().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+    Constants.driverController.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
+    Constants.driverController.rightBumper().onTrue(Commands.none());
+    // Constants.driverController.x().onTrue(Commands.runOnce(drivebase::addFakeVisionReading));
+    // Constants.driverController.b().whileTrue(drivebase.driveToPose(new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0))));
+
+    // Constants.driverController.x().onTrue(m_elevatorEnc.runElev(Constants.ElevatorSetpoints.L1SETPOINT)); // Keep Commeneted, req subsystem code isn't in github
+
+    // Shooter
+    Constants.operatorController.axisGreaterThan(1,0.1).onTrue(m_shooter.runShooter(0.25));
+    Constants.operatorController.axisLessThan(1,-0.1).onTrue(m_shooter.runShooter(-0.25));
+    // Elevator
+    Constants.operatorController.rightBumper().onTrue(m_elevator.runElevBtm()); // Left Bumper for Bottom
+    Constants.operatorController.povDown().onTrue(m_elevator.runElevL1()); // Down on the DPad for L1
+    Constants.operatorController.povLeft().onTrue(m_elevator.runElevL2()); // Left on the DPad for L2
+    Constants.operatorController.povRight().onTrue(m_elevator.runElevL3()); // Right on the DPad for L3
+    Constants.operatorController.povUp().onTrue(m_elevator.runElevL4()); // Up on the DPad for L4
+    // AprilTags 
+    // https://firstfrc.blob.core.windows.net/frc2025/FieldAssets/Apriltag_Images_and_User_Guide.pdf (pg 2 for map)
+    Constants.operatorController.b(); // Track close right (17/8) 
+    Constants.operatorController.a(); // Track close mid (18/7)
+    Constants.operatorController.x(); // Track close left (19/6)
+    Constants.operatorController.y(); // Track far right (22/9)
+    Constants.operatorController.rightBumper(); // Track far mid (21/10)
+    Constants.operatorController.rightTrigger(0.1); // Track far left (20/11)
+  }
+
+  /**
+   * Register the auto Commands
+   */
+  public void pathplannerCommands() {
+    NamedCommands.registerCommand("Elevator", null);
+  }
+
+  public Command getAutonomousCommand()
+  {
+    // Use the auto selected on the dashboard
+    return autoChooser.getSelected();
+  }
+
+  public void setMotorBrake(boolean brake)
+  {
+    drivebase.setMotorBrake(brake);
+  }
+
+  /* 
+   * ===========================================================================================================
+   *                                    We are getting into the swerve area
+   * ===========================================================================================================
+   */
 
   /**
    * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular velocity.
@@ -84,62 +154,6 @@ public class RobotContainer {
       () -> Math.cos(driverXbox.getRawAxis(2) * Math.PI) * (Math.PI * 2))
     .headingWhile(true);
 
-  public RobotContainer()
-  {
-    // Get our auto commands.
-    pathplannerCommands();
-
-    DriverStation.silenceJoystickConnectionWarning(true);
-
-    // Create the autochooser for the driver station dashboard
-    autoChooser = AutoBuilder.buildAutoChooser("New Path");
-    SmartDashboard.putData("Auto Mode", autoChooser);
-
-    // Forward port 5800 for photonvision
-    PortForwarder.add(5800, "photonvision.local", 5800);
-
-    // Configure the trigger bindings
-    configureBindings();
-  }
-
-  private void configureBindings() {
-    setDriveMode();
-
-    Constants.driverController.a().onTrue((Commands.runOnce(drivebase::zeroGyro)));
-    Constants.driverController.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
-    Constants.driverController.rightBumper().onTrue(Commands.none());
-    // Constants.driverController.x().onTrue(Commands.runOnce(drivebase::addFakeVisionReading));
-    // Constants.driverController.b().whileTrue(drivebase.driveToPose(new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0))));
-
-    // Constants.driverController.x().onTrue(m_elevatorEnc.runElev(Constants.ElevatorSetpoints.L1SETPOINT)); // Keep Commeneted, req subsystem code isn't in github
-
-    // Shooter
-    Constants.operatorController.axisGreaterThan(1,0.1).onTrue(m_shooter.runShooter(0.25));
-    Constants.operatorController.axisLessThan(1,-0.1).onTrue(m_shooter.runShooter(-0.25));
-    // Elevator
-    Constants.operatorController.rightBumper().onTrue(m_elevator.runElevBtm()); // Left Bumper for Bottom
-    Constants.operatorController.povDown().onTrue(m_elevator.runElevL1()); // Down on the DPad for L1
-    Constants.operatorController.povLeft().onTrue(m_elevator.runElevL2()); // Left on the DPad for L2
-    Constants.operatorController.povRight().onTrue(m_elevator.runElevL3()); // Right on the DPad for L3
-    Constants.operatorController.povUp().onTrue(m_elevator.runElevL4()); // Up on the DPad for L4
-    // AprilTags 
-    // https://firstfrc.blob.core.windows.net/frc2025/FieldAssets/Apriltag_Images_and_User_Guide.pdf (pg 2 for map)
-    Constants.operatorController.b().onTrue(Commands.runOnce(null)); // Track close right (17/8) 
-    Constants.operatorController.a(); // Track close mid (18/7)
-    Constants.operatorController.x(); // Track close left (19/6)
-    Constants.operatorController.y(); // Track far right (22/9)
-    Constants.operatorController.rightBumper(); // Track far mid (21/10)
-    Constants.operatorController.rightTrigger(0.1); // Track far left (20/11)
-  }
-
-  /**
-   * Register the auto Commands
-   */
-  public void pathplannerCommands() {
-    NamedCommands.registerCommand("Elevator", null);
-  }
-
-
   /**
    * Try this to see if I can clear space in configureBindings
    * 
@@ -164,16 +178,5 @@ public class RobotContainer {
       driverXbox.start().onTrue(Commands.runOnce(() -> drivebase.resetOdometry(new Pose2d(3, 3, new Rotation2d()))));
       driverXbox.button(1).whileTrue(drivebase.sysIdDriveMotorCommand());
     }
-  }
-
-  public Command getAutonomousCommand()
-  {
-    // Use the auto selected on the dashboard
-    return autoChooser.getSelected();
-  }
-
-  public void setMotorBrake(boolean brake)
-  {
-    drivebase.setMotorBrake(brake);
   }
 }
