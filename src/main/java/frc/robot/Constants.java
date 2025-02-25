689402600b880aa50b9733185dc8a456291bf2f7
Ryu <65792780+RyuIsStrange@users.noreply.github.com>
2025-02-25 10:34:46 -0600

    remove unused

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import java.util.Optional;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
